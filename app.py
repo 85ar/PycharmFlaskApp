7687d84c034f80dfe60534be156284a1e3423a7c
Artur Valeev <85ar@rambler.ru>
2020-02-13 23:21:33 +0500

    Двигаемся дальше3

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('auth.html')
 
 @app.route('/names')
 def names():
